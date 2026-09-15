92adb861f8c80875ff19994ad72a56aca4c5cf0d
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-09-15 07:33:35 +0700

    fix: harden Result state access

--- include/cpp_request/result.hpp
+++ include/cpp_request/result.hpp
@@ -12,14 +12,31 @@ namespace cpp_request {
 template <typename T>
 class [[nodiscard]] Result {
 public:
-    Result(T value)
-        : storage_(std::move(value)) {}
+    static_assert(!std::is_void_v<T>, "Result<void> is not implemented yet");
+
+    template <
+        typename U,
+        std::enable_if_t<
+            std::is_constructible_v<T, U&&>
+                && !std::is_same_v<std::decay_t<U>, Error>
+                && !std::is_same_v<T, Error>,
+            int> = 0>
+    Result(U&& value)
+        : storage_(std::in_place_index<0>, std::forward<U>(value)) {}
 
     Result(Error error)
-        : storage_(error) {}
+        : storage_(std::in_place_index<1>, error) {}
+
+    [[nodiscard]] static Result success(T value) {
+        return Result{ValueTag{}, std::move(value)};
+    }
+
+    [[nodiscard]] static Result failure(Error error) {
+        return Result{ErrorTag{}, error};
+    }
 
     [[nodiscard]] bool has_value() const noexcept {
-        return std::holds_alternative<T>(storage_);
+        return storage_.index() == 0;
     }
 
     [[nodiscard]] explicit operator bool() const noexcept {
@@ -27,31 +44,45 @@ public:
     }
 
     T& value() & noexcept {
-        assert(has_value());
-        return std::get<T>(storage_);
+        auto* value = std::get_if<0>(&storage_);
+        assert(value != nullptr);
+        return *value;
     }
 
     const T& value() const& noexcept {
-        assert(has_value());
-        return std::get<T>(storage_);
+        const auto* value = std::get_if<0>(&storage_);
+        assert(value != nullptr);
+        return *value;
     }
 
     T&& value() && noexcept {
-        assert(has_value());
-        return std::get<T>(std::move(storage_));
+        auto* value = std::get_if<0>(&storage_);
+        assert(value != nullptr);
+        return std::move(*value);
     }
 
     Error& error() & noexcept {
-        assert(!has_value());
-        return std::get<Error>(storage_);
+        auto* error = std::get_if<1>(&storage_);
+        assert(error != nullptr);
+        return *error;
     }
 
     const Error& error() const& noexcept {
-        assert(!has_value());
-        return std::get<Error>(storage_);
+        const auto* error = std::get_if<1>(&storage_);
+        assert(error != nullptr);
+        return *error;
     }
 
 private:
+    struct ValueTag final {};
+    struct ErrorTag final {};
+
+    Result(ValueTag, T value)
+        : storage_(std::in_place_index<0>, std::move(value)) {}
+
+    Result(ErrorTag, Error error)
+        : storage_(std::in_place_index<1>, error) {}
+
     std::variant<T, Error> storage_;
 };
 
