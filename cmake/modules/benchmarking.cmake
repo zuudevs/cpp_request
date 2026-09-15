dcd7a9a4db48a8a801d70e3a52f26eb16cb15100
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-09-15 09:17:49 +0700

    build: centralize target helpers

--- cmake/modules/benchmarking.cmake
+++ cmake/modules/benchmarking.cmake
@@ -1,10 +1,28 @@
-include(${CMAKE_SOURCE_DIR}/cmake/packages/google-benchmark.cmake)
-	
-function(add_benchmark_target name)
-	add_executable(${name} ${ARGN})
-	target_link_libraries(${name} 
-		PRIVATE 
-			benchmark::benchmark_main
-			${PROJECT_NAME}::internal
+include_guard(GLOBAL)
+
+include("${PROJECT_SOURCE_DIR}/cmake/packages/google-benchmark.cmake")
+find_package(Threads REQUIRED)
+
+function(cpp_request_add_benchmark target source)
+	add_executable(${target} ${source})
+
+	target_include_directories(${target}
+		PRIVATE
+			${PROJECT_SOURCE_DIR}/src
+			${PROJECT_SOURCE_DIR}/benchmarks
 	)
+
+	target_link_libraries(${target}
+		PRIVATE
+			cpp_request::cpp_request
+			benchmark::benchmark_main
+			Threads::Threads
+	)
+
+	if(WIN32)
+		target_link_libraries(${target} PRIVATE ws2_32)
+	endif()
+
+	target_compile_features(${target} PRIVATE cxx_std_17)
+	cpp_request_configure_target(${target})
 endfunction()
--- cmake/modules/testing.cmake
+++ cmake/modules/testing.cmake
@@ -1,11 +1,28 @@
-include("${CMAKE_SOURCE_DIR}/cmake/packages/google-test.cmake")
+include_guard(GLOBAL)
 
-function(add_test_target name)
-	add_executable(${name} ${ARGN})
-	target_link_libraries(${name} 
-		PRIVATE 
-			GTest::gtest_main
-			${PROJECT_NAME}::internal
+include("${PROJECT_SOURCE_DIR}/cmake/packages/google-test.cmake")
+find_package(Threads REQUIRED)
+
+function(cpp_request_add_test target)
+	add_executable(${target} ${ARGN})
+
+	target_include_directories(${target}
+		PRIVATE
+			${PROJECT_SOURCE_DIR}/src
 	)
-	gtest_discover_tests(${name})
+
+	target_link_libraries(${target}
+		PRIVATE
+			cpp_request::cpp_request
+			GTest::gtest_main
+			Threads::Threads
+	)
+
+	if(WIN32)
+		target_link_libraries(${target} PRIVATE ws2_32)
+	endif()
+
+	target_compile_features(${target} PRIVATE cxx_std_17)
+	cpp_request_configure_target(${target})
+	gtest_discover_tests(${target})
 endfunction()
